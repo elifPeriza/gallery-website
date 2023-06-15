6abdb890aad3c444bed59b03ba813003c7e07de2
elif <elifg07@gmail.com>
2023-06-15 19:34:04 +0200

    Add fonts

---
+++ src/app/fonts.ts
@@ -0,0 +1,11 @@
+import { Fraunces, Roboto_Flex } from 'next/font/google'
+ 
+export const fraunces = Fraunces({
+  subsets: ['latin'],
+  display: 'swap',
+})
+ 
+export const roboto_flex = Roboto_Flex({
+  subsets: ['latin'],
+  display: 'swap',
+})
