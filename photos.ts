24ac299862d822048c00e77738e36329a5ef3843
elif <elifg07@gmail.com>
2023-06-17 23:25:27 +0200

    Update photos data

--- photos.ts
+++ photos.ts
@@ -14,6 +14,11 @@ const photos = [
     imageSrc:
       "https://images.unsplash.com/photo-1613149355673-c9bc2f590071?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80",
   },
+  {
+    id: "4",
+    imageSrc:
+      "https://images.unsplash.com/photo-1444021465936-c6ca81d39b84?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2039&q=80",
+  },
 ];
 
 export default photos;
